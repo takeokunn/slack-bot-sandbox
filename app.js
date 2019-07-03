cbfe7df66cbbc2d2721a20d132761eaeac1c2bdc
takeokunn <bararararatty@gmail.com>
2019-07-03 19:53:03 +0900

    UPdate

--- app.js
+++ app.js
@@ -1,23 +1,53 @@
+const slack = require('slack');
 const dotenv = require('dotenv');
 const express = require('express');
-const { WebClient } = require('@slack/web-api');
+const businesh = require('businesh');
+const bodyParser = require('body-parser');
 
 dotenv.config();
 
+const app = express();
 const config = {
     slack_token: process.env.SLACK_TOKEN,
     slack_verify_token: process.env.SLACK_VERIFY_TOKEN
 };
 
-const app = express();
-const web = new WebClient(config.slack_token);
+const postMessage = (channel_id, text) => {
+    slack.chat.postMessage({
+        token: config.slack_token,
+        channel: channel_id,
+        text: text
+    });
+};
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const handleTakebot = channel_id => postMessage(channel_id, "test");
+
+const handleBusinesh = (before_text, channel_id) => {
+    businesh.translate(before_text)
+        .then(res => postMessage(channel_id, res));
+};
+
+
+app.use(bodyParser());
+
+app.get('/', (_, res) => res.send('Hello World!'));
 
 app.post('/webhook', async (req, res) => {
-    // const res = await web.chat.postMessage({ channel: conversationId, text: 'Hello there' });
-    await res.send("hello world").status(200);
+    const text = await req.body.text;
+    // const token = await req.body.token;
+    const command = await req.body.command;
+    const channel_id = await req.body.channel_id;
+    switch(command) {
+    case 'takebot':
+        await handleTakebot(channel_id);
+        break;
+    case 'businesh':
+        await handleBusinesh(text, channel_id);
+        break;
+    }
+    await res.status(200).send('good');
 });
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Node app is running at localhost: ${port}`));
