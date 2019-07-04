2dd4b75ebbe7c1d786527d199d612d05e8113baa
takeokunn <bararararatty@gmail.com>
2019-07-04 14:25:15 +0900

    Update

--- app.js
+++ app.js
@@ -81,7 +81,6 @@ app.get('/', (_, res) => res.send('Hello World!'));
 
 app.post('/webhook', async (req, res) => {
     const text = await req.body.text;
-    const token = await req.body.token;
     const command = await req.body.command;
     const channel_id = await req.body.channel_id;
 
