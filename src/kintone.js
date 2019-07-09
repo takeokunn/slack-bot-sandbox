d1bea44394b5aec0f0c2ea35124af725703470ed
takeokunn <bararararatty@gmail.com>
2019-07-09 19:08:42 +0900

    Update

--- src/kintone.js
+++ src/kintone.js
@@ -2,9 +2,12 @@ const { Auth, Connection, Record, App } = require('@kintone/kintone-js-sdk');
 
 const { postMessage } = require('./utils');
 
+const username = process.env.KINTONE_USERNAME;
+const password = process.env.KINTONE_PASSWORD;
+const domain = process.env.KINTONE_DOMAIN;
+const app_id = process.env.KINTONE_APP_ID;
+
 const handleKintoneAuth = () => {
-    const username = process.env.KINTONE_USERNAME;
-    const password = process.env.KINTONE_PASSWORD;
     const auth = new Auth();
     auth.setPasswordAuth(username, password);
     return auth;
@@ -12,31 +15,18 @@ const handleKintoneAuth = () => {
 
 const fetchSearchLatestRecord = (record, app_id, query, success, failure) => {
     record.getRecords(app_id, query)
-        .then((rsp) => {
-            success(rsp);
-        })
-        .catch((err) => {
-            // This SDK return err with KintoneAPIExeption
-            failure(err);
-        });
+        .then((res) => success(res.records[0]))
+        .catch((err) => failure(err));
 };
 
-const handleKintone = async (text, channel_id) => {
-    const domain = process.env.KINTONE_DOMAIN;
-    const app_id = process.env.KINTONE_APP_ID;
-
+const handleKintone = (text, channel_id) => {
     const auth = handleKintoneAuth();
     const conn = new Connection(domain, auth);
     const record = new Record(conn);
-    const app = new App(conn);
-
-    const success = data => {
-
-    };
-    const failure = err => {
-        console.log(err.get());
-    };
-    await fetchSearchLatestRecord(record, app_id);
+    const success = data => postMessage(channel_id, data.content.value);
+    const failure = err => console.log(err.get().errors);
+    const query = `Owner in ("${text}") limit 1`;
+    fetchSearchLatestRecord(record, app_id, query, success, failure);
 };
 
 module.exports = handleKintone;
