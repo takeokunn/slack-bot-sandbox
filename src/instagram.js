95d20ed7283b4f4d3324878fe74b74676d6b99bf
takeokunn <bararararatty@gmail.com>
2019-07-12 16:25:41 +0900

    Update

--- src/instagram.js
+++ src/instagram.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const { postMessage } = require('./utils');
 
 const fetchDetailByName = (name, success, failure) => {
-    const url = `https://www.instagram.com/${name}`;
+    const url = `https://www.instagram.com/${name}/`;
     axios.get(url)
         .then(res => success(JSON.parse(res.data.split("window._sharedData = ")[1].split(";</script>")[0])))
         .catch(err => failure(err));
@@ -15,20 +15,25 @@ const handleInstagram = (channel_id, text) => {
         const username = user.username;
         const biography = user.biography;
         const follow = user.edge_follow.count;
-        const followed = user.edge_followed.count;
+        const followed = user.edge_followed_by.count;
 
-        const text = `インスタグラム検索: ${text}
-id: ${id}
-名前: ${username}
-bio: ${biography}
-フォロー数: ${follow}
-フォロワー数: ${followed}`;
-        postMessage(channel_id, text);
+        const message = `\`\`\`
+instagram
+
+id       : ${id}
+名前      : ${username}
+bio      : ${biography}
+follow   : ${follow}
+follower : ${followed}
+\`\`\``;
+        postMessage(channel_id, message);
     };
     const failure = _ => {
-        const text = `インスタグラム検索: ${text}
-not found`;
-        postMessage(channel_id, text);
+        const message = `\`\`\`instagram
+検索: ${text}
+not found
+\`\`\``;
+        postMessage(channel_id, message);
     };
     fetchDetailByName(text, success, failure);
 };
