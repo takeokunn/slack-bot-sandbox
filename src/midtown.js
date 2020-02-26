dd2fb7b745796a20b7b2d49455e3c4b9bca49e42
takeokunn <bararararatty@gmail.com>
2020-02-26 12:15:27 +0900

    Update

--- src/midtown.js
+++ src/midtown.js
@@ -1,7 +1,7 @@
 const { postMessage } = require('./utils');
 
 const handleMidtown = channel_id => {
-    const image_url = "https://raw.githubusercontent.com/takeokunn/slack-bot-sandbox/master/assets/floor_map_29.png";
+    const image_url = "https://raw.githubusercontent.com/takeokunn/slack-bot-sandbox/master/assets/midtown29.jpg";
     postMessage(channel_id, image_url);
 };
 
